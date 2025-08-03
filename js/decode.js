4ae33a9c29cbc2d5e23e077b6598c6622606e362
Eduard Yevchenko <82526715+Exlizardium@users.noreply.github.com>
2025-08-03 15:00:27 +0000

    Fixed new issues with the pop-up, changed style

--- js/decode.js
+++ js/decode.js
@@ -2,6 +2,7 @@ const decodeReverseButton = document.querySelector(".decoder__button_reverse");
 const decodeBrailleButton = document.querySelector(".decoder__button_braille");
 const decodeJumbleButton = document.querySelector(".decoder__button_jumble");
 const popupText = document.querySelector(".popup-content__paragraph");
+const popup = document.querySelector(".popup-overlay");
 const inputText = document.querySelector(".decoder__input").value;
 
 function decodeMessage(type) {
